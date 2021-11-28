f6b7c6c39f353a1ef5e41c25362398cb12444112
coffmanjrp <coffmanjrp@gmail.com>
2021-11-27 23:25:46 -0800

    Fetch Real Estate Properties

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
+import { Property } from '../components';
 import { baseUrl, fetchApi } from '../utils/fetchApi';
 
 const Banner = ({
@@ -37,8 +38,8 @@ const Banner = ({
 );
 
 export default function Home({ propertiesForSale, propertiesForRent }) {
-  console.log({ propertiesForSale });
-  console.log({ propertiesForRent });
+  // console.log({ propertiesForSale });
+  // console.log({ propertiesForRent });
 
   return (
     <Box>
@@ -52,7 +53,11 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
         linkName="/search?purpose=for-rent"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
       />
-      <Flex flexWrap="wrap">{/* Fetch data */}</Flex>
+      <Flex flexWrap="wrap">
+        {propertiesForRent.map((property) => (
+          <Property key={property.id} property={property} />
+        ))}
+      </Flex>
       <Banner
         purpose="BUY A HOME"
         title1=" Find, Buy &amp; Own Your"
@@ -63,7 +68,11 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
         linkName="/search?purpose=for-sale"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
       />
-      {/* Fetch data */}
+      <Flex flexWrap="wrap">
+        {propertiesForSale.map((property) => (
+          <Property key={property.id} property={property} />
+        ))}
+      </Flex>
     </Box>
   );
 }
