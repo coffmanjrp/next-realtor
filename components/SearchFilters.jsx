751d6c3c0cb2ea0e8a8ad5a164c8ef6b4d3d2296
coffmanjrp <coffmanjrp@gmail.com>
2021-11-29 18:40:10 -0800

    Create Search Page

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import {
+  Flex,
+  Select,
+  Box,
+  Text,
+  Input,
+  Spinner,
+  Icon,
+  Button,
+} from '@chakra-ui/react';
+import { MdCancel } from 'react-icons/md';
+
+const SearchFilters = () => {
+  return <div>SearchFilters</div>;
+};
+
+export default SearchFilters;
--- components/index.js
+++ components/index.js
@@ -2,3 +2,4 @@ export { default as Footer } from './Footer';
 export { default as Layout } from './Layout';
 export { default as Navbar } from './Navbar';
 export { default as Property } from './Property';
+export { default as SearchFilters } from './SearchFilters';
